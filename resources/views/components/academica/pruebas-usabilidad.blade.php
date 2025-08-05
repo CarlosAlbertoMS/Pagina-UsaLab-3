bc3a842cdc1b954e96d53384504ab84ea9eb6301
jjvdc22003 <jjvdc22003@gmail.com>
2025-08-05 10:04:56 -0600

    Fix: Redirección de las tarjetas esas

--- resources/views/components/academica/pruebas-usabilidad.blade.php
+++ resources/views/components/academica/pruebas-usabilidad.blade.php
@@ -10,7 +10,7 @@
     <div class="tarjetas">
         <!----------------Las tarjetas esas >;v ------------------------>
         <div class="contenedorTar">
-            <div class="tarjeta">
+            <div class="tarjeta" onclick="window.location.href='{{ url('/academica#pruebasEnLaboratorio') }}'">
                 <img src="{{ asset('img/FA_PU_01.png') }}" alt="" style="object-fit:cover;">
                 <div class="contenidoTar">
                     <!-- Curvatura azul que es el fondo del titulo -->
@@ -38,7 +38,7 @@
                 </div>
             </div>
 
-            <div class="tarjeta">
+            <div class="tarjeta" onclick="window.location.href='{{ url('/academica#pruebasDeUsabilidadConPrototipos') }}'">
                 <!-- Imagen de fondo que aparecerá en la card -->
                 <img src="{{ asset('img/FA_PU_02.png') }}" alt="">
                 <div class="contenidoTar">
@@ -67,7 +67,7 @@
                 </div>
             </div>
 
-            <div class="tarjeta">
+            <div class="tarjeta" onclick="window.location.href='{{ url('/academica#evaluacionHeuristica') }}'">
                 <!-- Imagen de fondo de la card -->
                 <img src="{{ asset('img/FA_PU_03.png') }}" alt="">
                 <div class="contenidoTar">
@@ -102,7 +102,7 @@
 
 
     <!-- Seccion Pruebas en laboratorio -->
-    <div class="pruebaslaboratorio">
+    <div id="pruebasEnLaboratorio" class="pruebaslaboratorio">
         <div class="pruebaslaboratoriotitle">Pruebas en Laboratorio</div>
         <div class="pruebaslaboratoriotxt">
             <div class="linea2"></div>
@@ -114,7 +114,7 @@
     <div class="separador2"></div>
 
     <!-- Seccion Pruebas de Usabilidad con Prototipos -->
-    <div class="usabilidad">
+    <div id="pruebasDeUsabilidadConPrototipos" class="usabilidad">
         <div class="usabilidadtitle">Pruebas de Usabilidad con Prototipos</div>
         <div class="usabilidadcont">
             <div class="usabilidadp">Permiten evaluar versiones preliminares de un producto, ya sean bocetos simples o modelos avanzados, antes de su desarrollo final. Este método ayuda a identificar problemas de usabilidad de forma temprana, permitiendo ajustes rápidos y eficaces en el diseño. Los usuarios interactúan con los prototipos para realizar tareas específicas, mientras los evaluadores observan sus comportamientos y dificultades. Esto permite refinar el producto según las necesidades y expectativas de los usuarios, optimizando la experiencia de uso antes de que el producto esté completamente desarrollado.</div>
@@ -125,7 +125,7 @@
     <div class="separador3"></div>
 
     <!-- Seccion Evaluacion Heuristica -->
-    <div class="heuristica">
+    <div id="evaluacionHeuristica" class="heuristica">
         <div class="heuristicatitle">Evaluación Heurística</div>
         <div class="heuristicacont">
             <div class="heuristicap">Es un método experto que analiza la interfaz de un producto o sistema mediante el uso de principios de usabilidad, llamados heurísticas. Este enfoque permite a especialistas en usabilidad identificar problemas de diseño de manera rápida y eficaz, sin la necesidad de pruebas directas con usuarios. Los evaluadores examinan la interfaz en busca de violaciones a estos principios clave, proporcionando recomendaciones para optimizar la experiencia del usuario y mejorar la interacción, todo antes de que el producto llegue a manos de los usuarios finales.</div>
